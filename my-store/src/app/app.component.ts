a41b0bd0875763f87e740387279fee78f9cc1780
pacifi <pacifi.bnr@gmail.com>
2022-12-27 07:09:44 -0500

    ngfor

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Product} from './producto.model'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   name = 'Bonnier';
+  newName = '';
   age = 18;
   img = 'https://w3schools.com/howto/img_avatar.png';
   btnDisable = true
@@ -15,7 +17,27 @@ export class AppComponent {
     age: 39,
     avatar: 'https://w3schools.com/howto/img_avatar.png'
   }
+  names: string[] = ['Juan', 'Maria', 'Terran'];
 
+  products: Product[] = [
+    {
+      name: 'robin',
+      price: 565,
+      image: './assets/images/robin1.jpg',
+      category: 'Juguete'
+    },
+    {
+      name: 'boa',
+      price: 50,
+      image: './assets/images/boa.jpg'
+    },
+    {
+      name: 'luffy',
+      price: 50,
+      image: './assets/images/luffy.jpg'
+    }
+
+  ];
 
   toggleButton() {
     this.btnDisable = !this.btnDisable;
@@ -33,8 +55,17 @@ export class AppComponent {
   }
 
   public changeName(event: Event) {
-    const  element = event.target as HTMLInputElement;
+    const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 
 }
