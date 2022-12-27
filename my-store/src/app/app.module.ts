28dc6b33f2d093547f7df9f5bca53d3fe6ac3dd0
pacifi <pacifi.bnr@gmail.com>
2022-12-27 15:05:51 -0500

    fundamentals 2

---
+++ my-store/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
+import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './components/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImgComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
